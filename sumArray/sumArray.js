def4380ef79f818aaaee20edba49b952d98c599c
= <nicolaij.scheu@gmx.de>
2023-12-19 14:01:00 +0100

    reduce also gives linear timecomplexity

--- sumArray/sumArray.js
+++ sumArray/sumArray.js
@@ -1,19 +1,24 @@
 const sumArray = function (array) {
-    const result = array.reduce((acc, curr) => {
-        return acc += curr
-    }, 0)
+    // reduce also gives linear time complexity
+
+    // const result = array.reduce((acc, curr) => {
+    //     return acc += curr
+    // }, 0)
+    // return result
+
+    let result = 0
+    for (const number of array) {
+        result += number
+    }
     return result
 }
 
 let arrayToSum = []
 
-for (let index = 0; index < 10000000; index++) {
-    arrayToSum.push(Math.floor(Math.random()*8))
-    
+for (let index = 0; index < 1000000; index++) {
+    arrayToSum.push(Math.floor(Math.random() * 8))
+
 }
-
-console.log(arrayToSum);
-
 
 const start = Date.now()
 console.log(sumArray(arrayToSum));
